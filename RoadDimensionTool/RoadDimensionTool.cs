00386edd23abf3559f4a49e075dee4072d8a2ca1
Cstough <32025644+Cstough@users.noreply.github.com>
2026-08-01 18:45:47 -0400

    - Renamed MapTool to RoadDimensionTool - Added boilerplate for creating the premade DimensionLine feature class that simply shows the line length as a centered label

---
+++ RoadDimensionTool/RoadDimensionTool.cs
@@ -0,0 +1,64 @@
+﻿using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoadDimensionTool
+{
+    internal class RoadDimensionTool : MapTool
+    {
+        public RoadDimensionTool()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Line;
+            SketchOutputMode = SketchOutputMode.Map;
+        }
+
+
+        protected override async Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            if (geometry is not Polyline line)
+                return false;
+
+            await CreateDimensionLine(line);
+
+            return true;
+        }
+
+
+        private async Task CreateDimensionLine(Polyline line)
+        {
+            await QueuedTask.Run(() =>
+            {
+                var layer = MapView.Active.Map
+                    .GetLayersAsFlattenedList()
+                    .OfType<FeatureLayer>()
+                    .FirstOrDefault(l => l.Name == "DimensionLine");
+
+                if (layer == null)
+                    return;
+
+
+                var editOperation = new EditOperation
+                {
+                    Name = "Create Road Dimension Line"
+                };
+
+
+                editOperation.Create(
+                    layer,
+                    line,
+                    new Dictionary<string, object>
+                    {
+                        { "DisplayLength", null }
+                    });
+
+
+                editOperation.Execute();
+            });
+        }
+    }
+}
