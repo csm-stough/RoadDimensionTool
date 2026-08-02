b2ac3d6ba730f21a173e240b111619569166a15a
Cstough <32025644+Cstough@users.noreply.github.com>
2026-08-01 21:49:05 -0400

    - Added local property list for the basemapping feature classes we want to check for when making dimensions - Created the initial logic for using the sketch line to find all intersection points of these predefined layers - Added inputs for a few other dimension line properties when they get created - Added a basic offset function that will be used to create the second set of "sub-measurement lines" when the time for that comes - Added function to ensure intersection points are correctly ordered from the starting point to ensure proper dimension line creation

--- RoadDimensionTool/RoadDimensionTool.cs
+++ RoadDimensionTool/RoadDimensionTool.cs
@@ -1,7 +1,10 @@
-﻿using ArcGIS.Core.Geometry;
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
 using ArcGIS.Desktop.Editing;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +13,12 @@ namespace RoadDimensionTool
 {
     internal class RoadDimensionTool : MapTool
     {
+
+        private readonly List<string> intersection_layers = new List<string>
+        {
+            "Right of Way", "Edge of Pavement", "Back of Curb", "Easement"
+        };
+
         public RoadDimensionTool()
         {
             IsSketchTool = true;
@@ -23,13 +32,26 @@ namespace RoadDimensionTool
             if (geometry is not Polyline line)
                 return false;
 
-            await CreateDimensionLine(line);
+            var dimensionId = Guid.NewGuid();
+
+            //await CreateDimensionLines(line, dimensionId);
+            await QueuedTask.Run(() =>
+            {
+                var points = FindIntersections(line);
+
+                var orderedIntersections = OrderIntersections(line, points);
+
+                foreach (var point in orderedIntersections)
+                {
+                    System.Diagnostics.Debug.WriteLine(point);
+                }
+            });
 
             return true;
         }
 
 
-        private async Task CreateDimensionLine(Polyline line)
+        private async Task CreateDimensionLines(Polyline line, Guid dimensionId)
         {
             await QueuedTask.Run(() =>
             {
@@ -47,18 +69,93 @@ namespace RoadDimensionTool
                     Name = "Create Road Dimension Line"
                 };
 
-
                 editOperation.Create(
                     layer,
                     line,
                     new Dictionary<string, object>
                     {
-                        { "DisplayLength", null }
+                        { "DisplayLength", null },
+                        { "DimID", dimensionId},
+                        { "DimSide", "C"}
                     });
 
+                // Offset measurement line
+                var offsetLine = OffsetLine(line, 20);
 
                 editOperation.Execute();
             });
+        }
+
+        private Polyline OffsetLine(Polyline line, double distance)
+        {
+            var geometry = GeometryEngine.Instance.Offset(
+                line,
+                distance,
+                OffsetType.Miter, 
+                1.0);
+
+            return geometry as Polyline;
+        }
+
+        private List<FeatureLayer> GetIntersectionLayers()
+        {
+            return MapView.Active.Map
+                .GetLayersAsFlattenedList()
+                .OfType<FeatureLayer>()
+                .Where(layer => intersection_layers.Contains(layer.Name))
+                .ToList();
+        }
+
+        private List<MapPoint> FindIntersections(Polyline sketch)
+        {
+            var intersections = new List<MapPoint>();
+
+            var layers = GetIntersectionLayers();
+
+            foreach (var layer in GetIntersectionLayers())
+            {
+                using var cursor = layer.Search();
+
+                while (cursor.MoveNext())
+                {
+                    var feature = cursor.Current as Feature;
+
+                    var intersection =
+                        GeometryEngine.Instance.Intersection(
+                            sketch,
+                            feature.GetShape(),
+                            GeometryDimensionType.EsriGeometry0Dimension);
+
+                    if (intersection is Multipoint multipoint)
+                    {
+                        intersections.AddRange(
+                            multipoint.Points);
+                    }
+                }
+            }
+
+            return intersections;
+        }
+
+        private List<MapPoint> OrderIntersections(
+            Polyline sketch,
+            List<MapPoint> points)
+        {
+            return points
+                .OrderBy(point =>
+                {
+                    GeometryEngine.Instance.QueryPointAndDistance(
+                        sketch,
+                        SegmentExtensionType.NoExtension,
+                        point,
+                        AsRatioOrLength.AsLength,
+                        out double distanceAlongCurve,
+                        out _,
+                        out _);
+
+                    return distanceAlongCurve;
+                })
+                .ToList();
         }
     }
 }
